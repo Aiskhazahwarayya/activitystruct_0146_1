affbda7729b5f05135d50230da4c6638c8392c9d
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-04-01 14:34:26 +0700

    input data mahasiswa

--- AcivityStruct/AcivityStruct.cpp
+++ AcivityStruct/AcivityStruct.cpp
@@ -10,7 +10,17 @@ struct Mahasiswa {
 
 int main()
 {
-   
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+
 }
 
 
